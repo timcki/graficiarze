6dcc90830be9fa0d62c2bbb76544cafa623fe91c
Tim <tymekch@pm.me>
2021-05-02 12:31:38 +0200

    formatting

--- 02_project/graph.py
+++ 02_project/graph.py
@@ -2,44 +2,62 @@ from typing import List, Any
 import numpy as np
 
 class Graph:
-	def __init__(self, adj: Any):
-		self.adjacency = adj
+    def __init__(self, adj: Any):
+        self.adjacency = adj
 
-	# Algorithms taken from here:
-	# https://math.stackexchange.com/questions/1074651/check-if-sequence-is-graphic-8-8-7-7-6-6-4-3-2-1-1-1
-	# https://mathworld.wolfram.com/GraphicSequence.html
-	@classmethod
-	def from_sequence(cls, seq: List[int]) -> Any:
+    # Algorithms taken from here:
+    # https://math.stackexchange.com/questions/1074651/check-if-sequence-is-graphic-8-8-7-7-6-6-4-3-2-1-1-1
+    # https://mathworld.wolfram.com/GraphicSequence.html
+    @classmethod
+    def from_sequence(cls, seq: List[int]) -> Any:
 
-		if sum(seq) % 2:
-			raise NotGraphicSequenceException()
+        if sum(seq) % 2:
+            raise NotGraphicSequenceException()
 
-		seq_copy = []
-		while True:
+        seq_copy = seq
+        while True:
 
-			seq_copy = -np.sort(-seq)
-			if seq_copy[0] == 0 and seq_copy[-1] == 0:
-				return adjacency_from_sequence(seq)
+            print(seq_copy)
+            seq_copy = -np.sort(-seq_copy)
+            if seq_copy[0] == 0 and seq_copy[-1] == 0:
+                return cls.adjacency_from_sequence(-np.sort(-seq))
 
-			k = seq_copy[0]
-			seq_copy = seq_copy[1:]
-			print(seq_copy)
+            k = seq_copy[0]
+            seq_copy = seq_copy[1:]
 
-			if k > len(seq_copy):
-				raise NotGraphicSequenceException()
+            if k > len(seq_copy):
+                raise NotGraphicSequenceException()
 
-			for i in range(k):
-				seq_copy[i] -= 1
-				if seq_copy[i] < 0:
-					raise NotGraphicSequenceException()
+            for i in range(k):
+                seq_copy[i] -= 1
+                if seq_copy[i] < 0:
+                    raise NotGraphicSequenceException()
 
-			print(seq_copy)
+            print(seq_copy)
 
 
-	def adjacency_from_sequence(seq: List[int]) -> Any:
-		return np.array(([1, 2, 3], [4, 5, 6]))
+    @classmethod
+    def adjacency_from_sequence(cls, seq: List[int]) -> Any:
+        a_matrix = np.zeros(shape=(len(seq), len(seq)), dtype=np.int8)
+        while True:
+
+            seq_copy = -np.sort(-seq_copy)
+            if seq_copy[0] == 0 and seq_copy[-1] == 0:
+                return cls.adjacency_from_sequence(-np.sort(-seq))
+
+            k = seq_copy[0]
+            seq_copy = seq_copy[1:]
+
+            for i in range(k):
+                seq_copy[i] -= 1
+                if seq_copy[i] < 0:
+                    raise NotGraphicSequenceException()
+
+
+        return cls(a_matrix)
+
 
 
 
 class NotGraphicSequenceException(Exception):
-	pass
+    pass
--- 02_project/main.py
+++ 02_project/main.py
@@ -6,11 +6,12 @@ import random
 import numpy as np
 
 def main():
-	tries = 500
+	tries = 1
 	for i in range(tries):
 		el = np.array([random.randint(0, 9) for x in range(25)])
 		try:
 			g = Graph.from_sequence(el)
+			print(g.adjacency)
 		except NotGraphicSequenceException:
 			print("Not a graphic sequence")
 
