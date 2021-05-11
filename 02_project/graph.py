d27c2cb2057b706750d52031ce231af3a45790a6
Tim <tymekch@pm.me>
2021-05-11 18:36:44 +0200

    Not perfect. Semi-working randomization ok

--- 02_project/graph.py
+++ 02_project/graph.py
@@ -1,9 +1,12 @@
 from typing import List, Any
+
+import random
 import numpy as np
 
 class Graph:
     def __init__(self, adj: Any):
         self.adjacency = adj
+        self.size = adj.shape[0]
 
     # Algorithms taken from here:
     # https://math.stackexchange.com/questions/1074651/check-if-sequence-is-graphic-8-8-7-7-6-6-4-3-2-1-1-1
@@ -16,11 +19,9 @@ class Graph:
 
         seq_copy = seq
         while True:
-
-            print(seq_copy)
             seq_copy = -np.sort(-seq_copy)
             if seq_copy[0] == 0 and seq_copy[-1] == 0:
-                return cls.adjacency_from_sequence(-np.sort(-seq))
+                return cls.adjacency_from_sequence(seq)
 
             k = seq_copy[0]
             seq_copy = seq_copy[1:]
@@ -33,28 +34,44 @@ class Graph:
                 if seq_copy[i] < 0:
                     raise NotGraphicSequenceException()
 
-            print(seq_copy)
-
 
     @classmethod
     def adjacency_from_sequence(cls, seq: List[int]) -> Any:
+
         a_matrix = np.zeros(shape=(len(seq), len(seq)), dtype=np.int8)
-        while True:
+        pos = 0
 
-            seq_copy = -np.sort(-seq_copy)
-            if seq_copy[0] == 0 and seq_copy[-1] == 0:
-                return cls.adjacency_from_sequence(-np.sort(-seq))
+        seq = -np.sort(-seq)
+        while len(seq) > 0:
 
-            k = seq_copy[0]
-            seq_copy = seq_copy[1:]
+            k = seq[0]
+            print(k)
+            seq = seq[1:]
+            if k < 1:
+                continue
 
-            for i in range(k):
-                seq_copy[i] -= 1
-                if seq_copy[i] < 0:
-                    raise NotGraphicSequenceException()
+            for i in range(1, k+1):
+                seq[i-1] -= 1
+                a_matrix[pos][i+pos] = a_matrix[i+pos][pos] = 1
 
+            pos += 1
 
         return cls(a_matrix)
+
+    def randomize_edges(self):
+        while True:
+            p1 = (random.randint(1, self.size-1), random.randint(1, self.size-1))
+            if self.adjacency[p1[0]][p1[1]] == 1:
+                break
+        while True:
+            p2 = (random.randint(1, self.size-1), random.randint(1, self.size-1))
+            if self.adjacency[p2[0]][p2[1]] == 1:
+                break
+        self.adjacency[p1[0]][p2[1]] = 1
+        self.adjacency[p2[0]][p1[1]] = 1
+        self.adjacency[p1[0]][p1[1]] = 0
+        self.adjacency[p2[0]][p2[1]] = 0
+        print(p1, p2)
 
 
 
--- 02_project/main.py
+++ 02_project/main.py
@@ -6,16 +6,21 @@ import random
 import numpy as np
 
 def main():
-	tries = 1
-	for i in range(tries):
-		el = np.array([random.randint(0, 9) for x in range(25)])
-		try:
-			g = Graph.from_sequence(el)
-			print(g.adjacency)
-		except NotGraphicSequenceException:
-			print("Not a graphic sequence")
+    tries = 1
+    for i in range(tries):
+        el = np.array([random.randint(0, 9) for x in range(25)])
+        try:
+            print(-np.sort(-el))
+            g = Graph.from_sequence(el)
+            g.randomize_edges()
+            for l in g.adjacency:
+                for el in l:
+                    print(el, end=',')
+                print()
+        except NotGraphicSequenceException:
+            print("Not a graphic sequence")
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
